63dc01fee9810c89fc9e1222ee3a5606f8102bcf
shaarifg <ermohdsharif@gmail.com>
2024-01-31 23:08:18 +0530

    app run cmd

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,6 @@
 const express = require("express");
-// const fileUpload = require("express-fileupload");
-// const db = require("./db");
 const cors = require('cors');
-
 const chartRoutes= require('./routes/growth.routes')
-
 const app = express();
 app.use(cors())
 
@@ -15,14 +11,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api', chartRoutes);
 
 
-
-
-//database connection method
-// db.createDbConnection();
-
-//posts related routes
-// const postRoutes = require("./routes/posts.routes");
-// app.use("", postRoutes);
 
 //starting server
 const PORT = process.env.PORT || 8080;
